aa285572c15cadddd97e9e1257388985625d583a
kripu77 <khadkakripu4@gmail.com>
2026-01-28 17:33:44 +1100

    barell exports

---
+++ app/components/thinkix/index.ts
@@ -0,0 +1,3 @@
+export { ThinkixBoard } from './ThinkixBoard';
+export { ThinkixToolbar } from './ThinkixToolbar';
+export { ThinkixProvider, useThinkix, type ThinkixTool, type ThinkixState } from './hooks/use-thinkix';
