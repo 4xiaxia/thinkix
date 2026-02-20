204c46b8b17b23505979892b203210c469acd10d
kripu77 <khadkakripu4@gmail.com>
2026-02-20 18:46:25 +1100

    barel exports

--- packages/plait-utils/lib/index.ts
+++ packages/plait-utils/lib/index.ts
@@ -1,23 +1,6 @@
-import { getSelectedElements } from '@plait/core';
-
-export function getSelectedMindElements(board: any) {
-  const selected = getSelectedElements(board);
-  return selected;
-}
-
-export function getCanvasContext(board: any): string {
-  const elements = board.children;
-  const selected = getSelectedElements(board);
-
-  return JSON.stringify({
-    elements,
-    selected: selected.map((e: any) => e.id),
-    viewport: board.viewport,
-  }, null, 2);
-}
-
-export function findElementById(board: any, id: string): any {
-  return board.children.find((e: any) => e.id === id) || null;
-}
-
-export { getSelectedElements } from '@plait/core';
+export { getSelectedElements, getSelectedMindElements, getCanvasContext, findElementById } from './board-utils';
+export { applyTextMark, applyTextColor, applyFontSize, getTextMarks } from './text-transforms';
+export type { TextMarks } from './text-transforms';
+export { getTextAccessor } from './text-accessor';
+export type { TextAccessor } from './text-accessor';
+export { asPlaitPlugin } from './plugin-utils';
